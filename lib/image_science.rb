7c241817f2d625aa3e1c8b961209738c2becd378
Ryan Davis <ryand@zenspider.com>
2009-06-23 18:44:57 -0800

    prepped for release [git-p4: depot-paths = "//src/image_science/dev/": change = 5210]

--- lib/image_science.rb
+++ lib/image_science.rb
@@ -11,7 +11,7 @@ require 'inline'
 # http://seattlerb.rubyforge.org/ImageScience.html
 
 class ImageScience
-  VERSION = '1.1.3'
+  VERSION = '1.2.0'
 
   ##
   # The top-level image loader opens +path+ and then yields the image.
